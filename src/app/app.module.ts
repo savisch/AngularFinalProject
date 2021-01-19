fce32b6597d5ed2d4bed6eb1f751921d6c55e84d
savisch <swsolns@gmail.com>
2021-01-18 20:31:41 -0600

    Create todo model and display/add/edit/detail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayTodoComponent } from './components/display-todo/display-todo.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { EditTodoComponent } from './components/edit-todo/edit-todo.component';
+import { DetailTodoComponent } from './components/detail-todo/detail-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayTodoComponent,
+    AddTodoComponent,
+    EditTodoComponent,
+    DetailTodoComponent
   ],
   imports: [
     BrowserModule,
