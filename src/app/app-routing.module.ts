2302d00095e0d2e24e2bc9b9662c229e1c563276
savisch <swsolns@gmail.com>
2021-01-20 11:15:54 -0600

    Add routes to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { DetailTodoComponent } from './components/detail-todo/detail-todo.component';
+import { DisplayTodoComponent } from './components/display-todo/display-todo.component';
+import { EditTodoComponent } from './components/edit-todo/edit-todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: '/display',
+    pathMatch: 'full'
+  },
+  {
+    path: "display",
+    component: DisplayTodoComponent
+  },
+  {
+    path: "add",
+    component: AddTodoComponent
+  },
+  {
+    path: "detail",
+    component: DetailTodoComponent
+  },
+  {
+    path: "edit",
+    component: EditTodoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
