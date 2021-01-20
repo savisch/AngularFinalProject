6d494d573c8c7bc9a479c2166ff631eea1488a81
savisch <swsolns@gmail.com>
2021-01-20 08:30:42 -0600

    Add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AddTodoComponent } from './components/add-todo/add-todo.component';
 import { EditTodoComponent } from './components/edit-todo/edit-todo.component';
 import { DetailTodoComponent } from './components/detail-todo/detail-todo.component';
 import { TodosService } from './services/todos.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { TodosService } from './services/todos.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [TodosService],
   bootstrap: [AppComponent]
