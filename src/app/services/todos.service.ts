03dd0bcc9662418cb7548235e00a3419fefbbb66
savisch <swsolns@gmail.com>
2021-01-20 11:16:30 -0600

    Add observable methods and HttpClient

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,10 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { Todo } from '../models/todo';
+import { parseTemplate } from '@angular/compiler';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
+ 
+  url: string = 'http://localhost:3000/todos';
 
-  constructor() { }
+
+  getTodos(): Observable<Todo[]>{
+    return this.http.get<Todo[]>(this.url);
+  };
+
+  getTodo(id: number): Observable<Todo>{
+    return this.http.get<Todo>(this.url + "/" + id)
+  };
+
+  addTodo(todo: Todo): Observable<Todo>{
+    return this.http.post<Todo>(this.url, todo);
+  };
+
+  editTodo(todo: Todo): Observable<Todo>{
+    return this.http.put<Todo>(this.url + "/" + todo.id, todo);
+  };
+
+  deleteTodo(id: number): Observable<Todo>{
+    return this.http.delete<Todo>(this.url + "/" + id);
+  };
+
+  constructor(private http: HttpClient) { };
 }
