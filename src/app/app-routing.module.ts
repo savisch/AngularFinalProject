5c09f47dfd8e7bc07cc8974d7ec6eeaa7292ed82
savisch <swsolns@gmail.com>
2021-01-22 14:34:42 -0600

    Build edit component with form and methods

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     component: DetailTodoComponent
   },
   {
-    path: "edit",
+    path: "edit/:id",
     component: EditTodoComponent
   }
 ];
--- src/app/components/edit-todo/edit-todo.component.ts
+++ src/app/components/edit-todo/edit-todo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Todo } from 'src/app/models/todo';
+import { TodosService } from '../../services/todos.service';
 
 @Component({
   selector: 'app-edit-todo',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-todo.component.css']
 })
 export class EditTodoComponent implements OnInit {
+  editTodo: Todo = new Todo();
+  editId: any;
 
-  constructor() { }
+  saveTodo(){
 
-  ngOnInit(): void {
   }
 
+  constructor(private todoService: TodosService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(param => {
+        this.editId = param.get('id');
+        this.todoService.getTodo(Number(this.editId)).subscribe(gottenTodo => 
+          this.editTodo = gottenTodo)
+    })
+  }
 }
